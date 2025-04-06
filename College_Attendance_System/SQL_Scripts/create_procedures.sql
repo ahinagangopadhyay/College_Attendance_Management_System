fe90c03cc57878c173213200271030c7dcc16eed
Ahina Gangopadhyay <159465464+ahinagangopadhyay@users.noreply.github.com>
2025-04-06 13:05:25 +0530

    Create create_procedures.sql

---
+++ College_Attendance_System/SQL_Scripts/create_procedures.sql
@@ -0,0 +1,17 @@
+--Q.9 Procedure stored to execute required query
+DELIMITER $$
+
+CREATE PROCEDURE GetLowestAttendanceSubject()
+BEGIN
+    SELECT 
+        a.SubjectID,
+        s.SubjectName,
+        ROUND(SUM(CASE WHEN a.Status = 'Present' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS AttendancePercentage
+    FROM Attendance a
+    JOIN Subjects s ON a.SubjectID = s.SubjectID
+    GROUP BY a.SubjectID, s.SubjectName
+    ORDER BY AttendancePercentage ASC
+    LIMIT 1;
+END$$
+
+DELIMITER ;
