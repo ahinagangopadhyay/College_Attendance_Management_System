b972b5c23a131d4437b7282f95550434ed749b17
Ahina Gangopadhyay <159465464+ahinagangopadhyay@users.noreply.github.com>
2025-04-06 16:49:26 +0530

    Create triggers.sql'

---
+++ College_Attendance_System/SQL_Scripts/triggers.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+CREATE TRIGGER NotifyNewAttendanceInsert
+AFTER INSERT ON Attendance
+FOR EACH ROW
+BEGIN
+    DECLARE msg TEXT;
+    SET msg = CONCAT(
+        'New attendance record inserted: StudentID ',
+        CAST(NEW.StudentID AS CHAR), ', SubjectID ',
+        CAST(NEW.SubjectID AS CHAR), ', Date ',
+        CAST(NEW.Date AS CHAR)
+    );
+
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = msg;
+END $$
+
+DELIMITER ;
