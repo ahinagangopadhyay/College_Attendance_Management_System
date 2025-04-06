94d83b837801eadf2a3d9d8c9bdf39f2f28b9eab
Ahina Gangopadhyay <159465464+ahinagangopadhyay@users.noreply.github.com>
2025-04-06 23:35:57 +0530

    Update triggers.sql

--- College_Attendance_System/SQL_Scripts/triggers.sql
+++ College_Attendance_System/SQL_Scripts/triggers.sql
@@ -1,20 +1,17 @@
---Additionally this trigger can be implemented to show a message everytime a new insertion is done
---Q.4
-    
+--Additionally this trigger can be implemented to show an error message everytime a new insertion is attempted if we want to prevent insertion
+
 DELIMITER $$
 
 CREATE TRIGGER NotifyNewAttendanceInsert
-AFTER INSERT ON Attendance
+BEFORE INSERT ON Attendance
 FOR EACH ROW
 BEGIN
     DECLARE msg TEXT;
-    SET msg = CONCAT(
-        'New attendance record inserted: StudentID ',
+    SET msg = CONCAT('New attendance record cannot be inserted: StudentID ',
         CAST(NEW.StudentID AS CHAR), ', SubjectID ',
         CAST(NEW.SubjectID AS CHAR), ', Date ',
         CAST(NEW.Date AS CHAR)
     );
-
     SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = msg;
 END $$
